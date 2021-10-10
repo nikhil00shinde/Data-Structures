e86569a1dd8c7f3fe550fbb7e08bce96e2ee2e6a
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-10-10 23:23:20 +0530

    memoization climb stair with minimum movies

--- 4.Dynamic Programming/4.ClimbStairsWithMinimumMoves.java
+++ 4.Dynamic Programming/4.ClimbStairsWithMinimumMoves.java
@@ -15,9 +15,41 @@ public class ClimbStairsWithMinimumMoves
             arr[i] = scn.nextInt();
         }
 
-        int ans = recursion(0,arr);
+        // int ans = recursion(0,arr);
+        int[] qb = new int[n+1];
+        Arrays.fill(qb,-1);
+        int ans = memoization(0,arr,qb);
         System.out.println(ans);
     }
+     
+
+    public static int memoization(int in,int[] arr,int[] qb)
+    {
+        if(in == arr.length)
+        {
+            return 0;
+        }
+
+        if(qb[in] != -1)
+        {
+            return qb[in];
+        }
+         
+         int min = Integer.MAX_VALUE;
+        for(int i=1;i<=arr[in] && i+in<=arr.length;i++)
+        {
+            min = Math.min(min,memoization(i+in,arr,qb));
+        }
+         qb[in] = min;
+        if(min == Integer.MAX_VALUE)
+        {
+            return qb[in];
+        }
+        qb[in] = min + 1;
+        return qb[in];
+
+    }
+
 
     public static int recursion(int in,int [] arr)
     {
